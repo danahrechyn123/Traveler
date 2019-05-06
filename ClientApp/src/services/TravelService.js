038b30f7bd821533d2e25e68a60b998980545dd4
dana.hrechyn <dana.hrechyn@gmail.com>
2019-05-07 00:06:34 +0300

    parsing all types

--- ClientApp/src/services/TravelService.js
+++ ClientApp/src/services/TravelService.js
@@ -7,6 +7,9 @@ export const travelService = {
 
 function addTravel(travel) {
     var travelType = parseTravelType(travel.travelType);
+    var placeType = parsePlaceType(travel.placeType);
+    var priceType = parsePriceType(travel.parsePriceType);
+
     console.log(travelType);
 }
 
@@ -17,3 +20,17 @@ function parseTravelType(typeName) {
     else if (typeName === "Educational") { return 2; }
     else if (typeName === "Bussiness") { return 3; }
 }
+function parsePlaceType(typeName) {
+    if (typeName === "Restaurant") { return 0; }
+    else if (typeName === "Hotel") { return 1; }
+    else if (typeName === "Museum") { return 2; }
+    else if (typeName === "Monument") { return 3; }
+    else if (typeName === "Entertaiment") { return 4; }
+}
+function parsePriceType(typeName) {
+    if (typeName === "Minimum") { return 0; }
+    else if (typeName === "Medium") { return 1; }
+    else if (typeName === "Expensive") { return 2; }
+    else if (typeName === "Luxury") { return 3; }
+}
+
